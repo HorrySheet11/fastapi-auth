fea42cb182fbede2a17a1d6eaf4f028fbb2f4079
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-04-30 21:07:02 +0800

    fixing: client not connected to query engine

--- fastApi-auth-backend/database.py
+++ fastApi-auth-backend/database.py
@@ -1,8 +1,8 @@
 from prisma import Prisma
 from fastapi import HTTPException
-from typing import Optional
 db = Prisma()
 
+# "Client is not connected to the query engine, you must call `connect()` before attempting to query data."
 async def is_db_connected() -> bool:
   try:
     await db.execute_raw('SELECT 1')
@@ -11,15 +11,13 @@ async def is_db_connected() -> bool:
     return False
   
 async def connect_db() -> None:
-  try:
     await db.connect()
-  except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
+
   
 async def get_user(email: str):
   try:
     if is_db_connected() == False:
-      await connect_db()
+      await db.connect()
     user = await db.user.find_unique(where={"email": email})
     return user
   except Exception as e:
@@ -27,10 +25,10 @@ async def get_user(email: str):
   finally:
     await db.disconnect()
 
-async def create_user(email: str, password ):
+async def create_user(email: str, password,  ):
     try:
       if is_db_connected() == False:
-        await connect_db()  
+        await db.connect()
       user = await db.user.create(data={
         "email": email, "password": password})
     except Exception as e:
--- fastApi-auth-backend/main.py
+++ fastApi-auth-backend/main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, Field
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 from database import get_user, create_user
+import bcrypt
 
 SECRET_KEY = "CHANGE_ME_IN_PRODUCTION"          # Use an env var in real apps
 ALGORITHM = "HS256"
@@ -16,23 +17,26 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class UserCreate(BaseModel):
-    username: str = Field(..., min_length=3, max_length=20)
+    email: str = Field(..., min_length=3, max_length=20)
     password: str = Field(..., min_length=6)
-    full_name: Optional[str] = None
 
 class UserPublic(BaseModel):
-    username: str
-    full_name: Optional[str] = None
+    email: str
 
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+def hash_password(password: str):
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt(rounds=12)  # rounds=12 is a good balance of security and performance
+    hashed = bcrypt.hashpw(password_bytes, salt)
+    return hashed
+
 
 def verify_password(plain: str, hashed: str) -> bool:
-    return pwd_context.verify(plain, hashed)
+    # return pwd_context.verify(plain, hashed)
+    return bcrypt.checkpw(plain.encode('utf-8'), hashed)
 
 def create_access_token(data: dict, expires_minutes: int = ACCESS_TOKEN_EXPIRE_MINUTES) -> str:
     to_encode = data.copy()
@@ -59,26 +63,25 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserPublic:
             raise cred_exc
     except JWTError:
         raise cred_exc
-
     user = get_user(user.email)
     if not user:
         raise cred_exc
-    return UserPublic(username=user["username"], full_name=user.get("full_name"))
+    return UserPublic(email=user["email"])
 
 
 # FIXME: ValueError: password cannot be longer than 72 bytes
 @app.post("/register", status_code=201, summary="Create a new user")
-def register_user(body: UserCreate):
+async def register_user(body: UserCreate):
     hashed = hash_password(body.password)
-    create_user(body.username, hashed, body.full_name or "")
+    await create_user(body.email, hashed)
     return {"message": "User registered successfully"}
 
 @app.post("/login", response_model=Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
+    user = authenticate_user(form_data.email, form_data.password)
     if not user:
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    access_token = create_access_token({"sub": user["username"]})
+    access_token = create_access_token({"sub": user["email"]})
     return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/me", response_model=UserPublic, summary="Get my profile (protected)")
